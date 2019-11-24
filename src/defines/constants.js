5b0546c243d17590c6d23d35f76ceac4b958cac0
mechanicore <mechanicoreframe@gmail.com>
2019-11-24 22:16:06 +0800

    Updated assets path for /gh-pages branch #21.

--- src/defines/constants.js
+++ src/defines/constants.js
@@ -1,7 +1,10 @@
+console.log('---DEFINES')
+console.log(process.env.NODE_ENV)
+
 module.exports = {
   GH_PAGES: {
     BASE_PATH: '/',
     REPOSITORY : '/mechanicore.ghpages',
   },
-  PRODUCTION: true,
+  PRODUCTION: process.env.NODE_ENV === 'production',
 }
