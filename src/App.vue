66d0faccd97999c6a0adbaa2bbef116a63a982a7
mechanicore <mechanicoreframe@gmail.com>
2019-11-24 16:16:25 +0800

    Initial commit. From @mechanicore.github.io

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  div#app
+    <img src="./assets/heavyarms.png"/>
+    
+    <router-view/>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  background-color: #f0f5f5;
+}
+</style>
